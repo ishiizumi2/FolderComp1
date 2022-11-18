ee6ccaf255c5da195ff5b2bc5d62d951d4aa4e2c
ishiizumi5 <di96dd@bma.biglobe.ne.jp>
2022-11-18 22:55:42 +0900

    2022/11/18

--- filecomp/FileCompForm.cs
+++ filecomp/FileCompForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using NPOI.SS.Formula.Functions;
 
 namespace filecomp
 {
@@ -62,14 +63,15 @@ namespace filecomp
                     return;
                 }
 
-                List<FileData> Read_file_list1 = new List<FileData>();
-                List<FileData> Read_file_list2 = new List<FileData>();
+                var Read_file_list1 = new List<FileData>();
+                var Read_file_list2 = new List<FileData>();
 
                 //ファイルから読み込んだテキストと行番号を代入する
                 foreach(var q3data in File.ReadLines(FileName1, SJIS).ToList().Select((d3,index) => new {d3 ,index}))
                 {
                     Read_file_list1.Add(new FileData(q3data.index+1, q3data.d3));
                 }
+
                 foreach (var q4data in File.ReadLines(FileName2, SJIS).ToList().Select((d4, index) => new { d4, index }))
                 {
                     Read_file_list2.Add(new FileData(q4data.index+1, q4data.d4));
@@ -100,48 +102,23 @@ namespace filecomp
                 {
                     Read_file_list_offset_position1 = sdata.index + Read_file_list_offset1;
                     Read_file_list_offset_position2 = sdata.index + Read_file_list_offset2;
+                    var searchtext1 = Read_file_list1[Read_file_list_offset_position1].File_Text;
+                    var searchtext2 = Read_file_list2[Read_file_list_offset_position2].File_Text;
 
                     if ((Read_file_list_offset_position1 < Read_file_list_count1) && (Read_file_list_offset_position2 < Read_file_list_count2))
                     {
-                        if (Read_file_list1[Read_file_list_offset_position1].Filetext == Read_file_list2[Read_file_list_offset_position2].Filetext) //テキスト一致
+                        if (searchtext1 == searchtext2) //テキスト一致
                         {
                             Change_file_list1.Add(Read_file_list1[Read_file_list_offset_position1]);
                             Change_file_list2.Add(Read_file_list2[Read_file_list_offset_position2]);
                         }
                         else //テキスト不一致　
                         {
-                            diff_location1 = 0;
-                            diff_location2 = 0;
-
-                            //skipで飛ばしてそれ以降のデータをListにする
-                            var results1 = Read_file_list2.Select(s => s.Filetext).Skip(Read_file_list_offset_position2).ToList();
-                            //resultsの中で検索する　検索するデータがnullでないこと resultsから見つからない場合nullを返す
-                            if (!(String.IsNullOrEmpty(Read_file_list1[Read_file_list_offset_position1].Filetext)) && (results1.FirstOrDefault(c => c == Read_file_list1[Read_file_list_offset_position1].Filetext) != null))
+                            diff_location2 = TextSearch(Read_file_list2,Read_file_list_offset_position2,searchtext1);
+ 
+                            if (diff_location2 == -1)//Read_file_list2から見つかっていないのでRead_file_list1から検索を行う
                             {
-                                index2 = results1.FindIndex(c => c == Read_file_list1[Read_file_list_offset_position1].Filetext);//query4から見つかった
-                            }
-                            else
-                                index2 = 0;
-
-                            if (index2 > 0)//Read_file_list2から見つかった
-                            {
-                                if (index2 < 100)//100以上なら差分が大きすぎて別の物と判断する
-                                    diff_location2 = index2;
-                            }
-                            else //Read_file_list2から見つかっていないのでRead_file_list1から検索を行う
-                            {
-                                //skipで飛ばしてそれ以降のデータをListにする
-                                var results2 = Read_file_list1.Select(s => s.Filetext).Skip(Read_file_list_offset_position1).ToList();
-                                //results2の中で検索する　検索するデータがnullでないこと results2から見つからない場合nullを返す
-                                if (!(String.IsNullOrEmpty(Read_file_list2[Read_file_list_offset_position2].Filetext)) && (results2.FirstOrDefault(c => c == Read_file_list2[Read_file_list_offset_position2].Filetext) != null))
-                                {
-                                    index3 = results2.FindIndex(c => c == Read_file_list2[Read_file_list_offset_position2].Filetext);
-                                }
-                                else
-                                    index3 = 0;
-
-                                if (index3 < 100) //100以上なら差分が大きすぎて別の物と判断する
-                                    diff_location1 = index3;
+                                diff_location1 = TextSearch(Read_file_list1, Read_file_list_offset_position1, searchtext2);
                             }
 
 
@@ -174,8 +151,8 @@ namespace filecomp
                             else 
                             if ((diff_location2 == 0) && (diff_location1 == 0))
                             {
-                                if (Read_file_list1.Count() > 1){
-                                   if (Read_file_list1[Read_file_list_offset_position1 + 1].Filetext == Read_file_list2[Read_file_list_offset_position2].Filetext)
+                                if (Read_file_list_count1 > 1){
+                                   if (Read_file_list1[Read_file_list_offset_position1 + 1].File_Text == searchtext2)
                                    {
                                        Change_file_list1.Add(Read_file_list1.ElementAtOrDefault(Read_file_list_offset_position1));
                                        Change_file_list1.Add(Read_file_list1.ElementAtOrDefault(Read_file_list_offset_position1 + 1));
@@ -184,7 +161,7 @@ namespace filecomp
                                        Read_file_list_offset1 = Read_file_list_offset1 + 1;
                                    }
                                    else
-                                   if (Read_file_list2[Read_file_list_offset_position2 + 1].Filetext == Read_file_list1[Read_file_list_offset_position1].Filetext)
+                                   if (Read_file_list2[Read_file_list_offset_position2 + 1].File_Text == searchtext1)
                                    {
                                        Change_file_list2.Add(Read_file_list2.ElementAtOrDefault(Read_file_list_offset_position2));
                                        Change_file_list2.Add(Read_file_list2.ElementAtOrDefault(Read_file_list_offset_position2 + 1));
@@ -208,50 +185,7 @@ namespace filecomp
                     }
                 }
 
-                dataGridView1.RowCount = Change_file_list1.Count();
-                //並び替えができないようにする
-                foreach (DataGridViewColumn c in dataGridView1.Columns)
-                    c.SortMode = DataGridViewColumnSortMode.NotSortable;
-
-                var query17 = from data in Change_file_list1.Zip(Change_file_list2, (data5, data6) => new { data5, data6 }) select data;
-                foreach(var pair in query17.Select((v4, index4) => new { v4, index4 }))
-                {
-                    var data5 = pair.v4.data5;
-                    var data6 = pair.v4.data6;
-
-                    dataGridView1[0, pair.index4].Value = data5.Filerowno;
-                    dataGridView1[1, pair.index4].Value = data5.Filetext;
-                    dataGridView1[2, pair.index4].Value = data6.Filerowno;
-                    dataGridView1[3, pair.index4].Value = data6.Filetext;
-
-                    if (data5.Filetext != data6.Filetext) //差分の箇所に色を塗る
-                    {
-                        if (data5.Filetext == " ")
-                        {
-                            dataGridView1[0, pair.index4].Style.BackColor = Color.LightGray;
-                            dataGridView1[1, pair.index4].Style.BackColor = Color.LightGray;
-                        }
-                        else
-                        {
-                            dataGridView1[0, pair.index4].Style.BackColor = Color.Yellow;
-                            dataGridView1[1, pair.index4].Style.BackColor = Color.Yellow;
-                        }
-                        if (data6.Filetext == " ")
-                        {
-                            dataGridView1[2, pair.index4].Style.BackColor = Color.LightGray;
-                            dataGridView1[3, pair.index4].Style.BackColor = Color.LightGray;
-                        }
-                        else
-                        {
-                            dataGridView1[2, pair.index4].Style.BackColor = Color.Yellow;
-                            dataGridView1[3, pair.index4].Style.BackColor = Color.Yellow;
-                        }
-                        RowDatas.Add(pair.index4 - 1); //差分1行前
-                        RowDatas.Add(pair.index4);     //差分行番号
-                        RowDatas.Add(pair.index4 + 1); //差分1行後 
-                    }
-                }
-                
+                Datagrildview_seikei(Change_file_list1, Change_file_list2);
             }
             catch
             {
@@ -259,7 +193,80 @@ namespace filecomp
             }
             
         }
-        
+
+        /// <summary>
+        /// ReadDFileListからTextを探す
+        /// </summary>
+        /// <param name="Read_file_list">対象読み込みファイルのList</param>
+        /// <param name="Read_file_list_offset_position">現在の行番号</param>
+        /// <param name="searchtext">検索行のText</param>
+        /// <returns>検索結果の行番号</returns>
+        private int TextSearch(List<FileData> Read_file_list, int Read_file_list_offset_position,string searchtext)
+        {
+           int position = 0;
+            //Read_file_list_offset_position分の行を飛ばして、それ以降のデータをListにする
+            var results = Read_file_list.Select(s => s.File_Text).Skip(Read_file_list_offset_position).ToList();
+
+            position = !(String.IsNullOrEmpty(searchtext)) && (results.FirstOrDefault(c => c == searchtext) != null) ? 
+                results.FindIndex(c => c == searchtext) : 0;//三項演算子
+
+            if (position > 100) //見つかった行が100行以上離れていたら別の行と判断する。
+            {
+                position = 0;
+            }
+
+            return position;
+        }
+
+
+        private void Datagrildview_seikei(List<FileData> Change_file_list1, List<FileData> Change_file_list2)
+        {
+            dataGridView1.RowCount = Change_file_list1.Count();
+            //並び替えができないようにする
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+                c.SortMode = DataGridViewColumnSortMode.NotSortable;
+
+            var query17 = from data in Change_file_list1.Zip(Change_file_list2, (data5, data6) => new { data5, data6 }) select data;
+            foreach (var pair in query17.Select((v4, index4) => new { v4, index4 }))
+            {
+                var data5 = pair.v4.data5;
+                var data6 = pair.v4.data6;
+
+                dataGridView1[0, pair.index4].Value = data5.File_Row_No;
+                dataGridView1[1, pair.index4].Value = data5.File_Text;
+                dataGridView1[2, pair.index4].Value = data6.File_Row_No;
+                dataGridView1[3, pair.index4].Value = data6.File_Text;
+
+                if (data5.File_Text != data6.File_Text) //差分の箇所に色を塗る
+                {
+                    if (data5.File_Text == " ")
+                    {
+                        dataGridView1[0, pair.index4].Style.BackColor = Color.LightGray;
+                        dataGridView1[1, pair.index4].Style.BackColor = Color.LightGray;
+                    }
+                    else
+                    {
+                        dataGridView1[0, pair.index4].Style.BackColor = Color.Yellow;
+                        dataGridView1[1, pair.index4].Style.BackColor = Color.Yellow;
+                    }
+                    if (data6.File_Text == " ")
+                    {
+                        dataGridView1[2, pair.index4].Style.BackColor = Color.LightGray;
+                        dataGridView1[3, pair.index4].Style.BackColor = Color.LightGray;
+                    }
+                    else
+                    {
+                        dataGridView1[2, pair.index4].Style.BackColor = Color.Yellow;
+                        dataGridView1[3, pair.index4].Style.BackColor = Color.Yellow;
+                    }
+                    RowDatas.Add(pair.index4 - 1); //差分1行前
+                    RowDatas.Add(pair.index4);     //差分行番号
+                    RowDatas.Add(pair.index4 + 1); //差分1行後 
+                }
+            }
+
+        }
+
         /// <summary>
         /// 差分の行のデータをファイに書き込む
         /// </summary>
@@ -374,14 +381,14 @@ namespace filecomp
     }
     class FileData
     {
-        public int Filerowno { get; private set; }
-        public string Filetext { get; private set; }
+        public int File_Row_No { get; private set; }
+        public string File_Text { get; private set; }
       
         public FileData() { }
-        public FileData(int filerowno,string filetext)
+        public FileData(int file_Row_No,string file_text)
         {
-            Filerowno = filerowno;
-            Filetext = filetext;
+            File_Row_No = file_Row_No;
+            File_Text = file_text;
         }
     }
     
